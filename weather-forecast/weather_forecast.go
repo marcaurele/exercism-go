9ea11d1795af44a0c863c0cc3c082ef2d2906684
Marc-Aurèle Brothier <m@brothier.org>
2025-04-04 23:01:07 +0200

    sol: comments

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides weather tools.
 package weather
 
+// CurrentCondition represents the current condition.
 var CurrentCondition string
+
+// CurrentLocation represents a location.
 var CurrentLocation string
 
+// Forecast returns a string for the weather forecast in city for the condition.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
