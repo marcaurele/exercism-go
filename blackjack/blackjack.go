3f12dce3dd1ac041ff78a0ccc62d6ffeb1645a54
Marc-Aurèle Brothier <m@brothier.org>
2025-04-13 22:31:32 +0200

    sol: conditionals

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -2,11 +2,45 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	switch card {
+	case "ace":
+		return 11
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	case "ten", "jack", "queen", "king":
+		return 10
+	default:
+		return 0
+	}
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	panic("Please implement the FirstTurn function")
+	switch sumCards := ParseCard(card1) + ParseCard(card2); {
+	case ParseCard(card1) == 11 && ParseCard(card2) == 11:
+		return "P"
+	case sumCards == 21 && ParseCard(dealerCard) < 10:
+		return "W"
+	case 12 <= sumCards && sumCards <= 16 && ParseCard(dealerCard) >= 7:
+		return "H"
+	case sumCards <= 11:
+		return "H"
+	default:
+		return "S"
+	}
 }
