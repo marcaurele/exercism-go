e9aaae870228e91145397b89cbb9889f92b992cd
Marc-Aurèle Brothier <m@brothier.org>
2025-04-20 20:10:34 +0200

    sol: functions

--- lasagna-master/lasagna_master.go
+++ lasagna-master/lasagna_master.go
@@ -1,18 +1,45 @@
 package lasagna
 
 // TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, timePerLayer int) int {
+	if timePerLayer == 0 {
+		timePerLayer = 2
+	}
+	return len(layers) * timePerLayer
+}
 
 // TODO: define the 'Quantities()' function
+func Quantities(layers []string) (noodles int, sauce float64) {
+	for i := 0; i < len(layers); i++ {
+		switch layers[i] {
+		case "sauce":
+			sauce += 0.2
+		case "noodles":
+			noodles += 50
+		}
+	}
+	return noodles, sauce
+}
 
 // TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendList, ownList []string) {
+	ownList[len(ownList)-1] = friendList[len(friendList)-1]
+}
 
 // TODO: define the 'ScaleRecipe()' function
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	scaledQuantities := make([]float64, 0, len(quantities))
+	for i := 0; i < len(quantities); i++ {
+		scaledQuantities = append(scaledQuantities, quantities[i]/2.0*float64(portions))
+	}
+	return scaledQuantities
+}
 
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.
 // The function body only needs to contain `panic("")`.
-// 
+//
 // This will make the tests compile, but they will fail.
 // You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
+// an increasing number of tests passing as you implement more
 // functionality.
