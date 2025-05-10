7c68939fa75aab6c71984624bd4f80045a2f83d8
Marc-Aurèle Brothier <m@brothier.org>
2025-05-10 08:54:42 +0200

    sol: runes

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,37 @@
 package logs
 
+import "unicode/utf8"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, char := range log {
+		if char == '❗' {
+			return "recommendation"
+		} else if char == '🔍' {
+			return "search"
+		} else if char == '☀' {
+			return "weather"
+		}
+	}
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	var newLog = []rune(log)
+	for index, char := range newLog {
+		if char == oldRune {
+			newLog[index] = newRune
+		} else {
+			newLog[index] = char
+		}
+	}
+	return string(newLog)
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return utf8.RuneCountInString(log) <= limit
 }
