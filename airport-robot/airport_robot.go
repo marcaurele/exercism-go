705545edbe7aa261d3db59fbf3de99401ea0e1eb
Marc-Aurèle Brothier <m@brothier.org>
2025-05-19 07:39:27 +0200

    sol: interfaces

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,5 +1,38 @@
 package airportrobot
 
+import "fmt"
+
 // Write your code here.
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+func SayHello(name string, greeter Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", greeter.LanguageName(), greeter.Greet(name))
+}
+
+type Italian struct {
+}
+
+func (italian Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (italian Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
+
+type Portuguese struct {
+}
+
+func (italian Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (italian Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s!", name)
+}
