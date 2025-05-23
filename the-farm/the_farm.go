92fd48c1bee20294a43911ea0a657797ca7a4f58
Marc-Aurèle Brothier <m@brothier.org>
2025-05-23 07:08:52 +0200

    sol: errors

--- the-farm/the_farm.go
+++ the-farm/the_farm.go
@@ -1,10 +1,54 @@
 package thefarm
 
+import (
+	"errors"
+	"fmt"
+)
+
 // TODO: define the 'DivideFood' function
+func DivideFood(fodderCalculator FodderCalculator, cows int) (float64, error) {
+	fodder, err := fodderCalculator.FodderAmount(cows)
+	if err != nil {
+		return 0, err
+	}
+
+	fattening, err := fodderCalculator.FatteningFactor()
+	if err != nil {
+		return 0, err
+	}
+
+	return float64(fodder) / float64(cows) * fattening, nil
+}
 
 // TODO: define the 'ValidateInputAndDivideFood' function
+func ValidateInputAndDivideFood(fodderCalculator FodderCalculator, cows int) (float64, error) {
+	if cows <= 0 {
+		return 0, errors.New("invalid number of cows")
+	}
+	return DivideFood(fodderCalculator, cows)
+}
 
 // TODO: define the 'ValidateNumberOfCows' function
+func ValidateNumberOfCows(cows int) error {
+	if cows < 0 {
+		return &InvalidCowsError{cows: cows, message: "there are no negative cows"}
+	}
+
+	if cows == 0 {
+		return &InvalidCowsError{cows: cows, message: "no cows don't need food"}
+	}
+
+	return nil
+}
+
+type InvalidCowsError struct {
+	cows    int
+	message string
+}
+
+func (e *InvalidCowsError) Error() string {
+	return fmt.Sprintf("%d cows are invalid: %s", e.cows, e.message)
+}
 
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.
