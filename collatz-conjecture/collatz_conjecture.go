a3be00ede115bf1d52c2cdf414086cb292181585
Marc-Aurèle Brothier <m@brothier.org>
2025-05-25 20:47:59 +0200

    sol: extra for loops

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,21 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return 0, errors.New("value must be greater than 0")
+	}
+
+	var steps int
+	for n != 1 {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+		steps++
+	}
+
+	return steps, nil
 }
