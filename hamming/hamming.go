385752cade38ba28a9337c2537f0e502733d759e
Marc-Aurèle Brothier <m@brothier.org>
2025-05-26 20:27:12 +0200

    sol: extra for loops

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,5 +1,18 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("both parameters must have the same length")
+	}
+
+	distance := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
