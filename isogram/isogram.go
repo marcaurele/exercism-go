6a074dcb99ca674e3d429bb728f4e2514929d1ac
Marc-Aurèle Brothier <m@brothier.org>
2025-05-27 17:49:14 +0200

    sol: extra runes, range

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,5 +1,15 @@
 package isogram
 
+import "strings"
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	letters := map[rune]bool{}
+	for _, letter := range strings.ToUpper(word) {
+		if _, exists := letters[letter]; exists {
+			return false
+		} else if letter != '-' && letter != ' ' {
+			letters[letter] = true
+		}
+	}
+	return true
 }
