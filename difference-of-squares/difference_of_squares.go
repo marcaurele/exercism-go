e79fa97f9d6549ac380b9d8b9675be6bb898b7bc
Marc-Aurèle Brothier <m@brothier.org>
2025-05-28 09:04:08 +0200

    sol: algo - optim #2

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,19 +1,14 @@
 package diffsquares
 
-import "math"
-
 func SquareOfSum(n int) int {
-	total := (n + 1) * (n / 2)
-	if n%2 == 1 {
-		total += (n + 1) / 2
-	}
-	return int(math.Pow(float64(total), 2))
+	total := (n * (n + 1) / 2)
+	return total * total
 }
 
 func SumOfSquares(n int) int {
 	total := 0
 	for i := 0; i < n; i++ {
-		total += int(math.Pow(float64(i+1), 2))
+		total += (i + 1) * (i + 1)
 	}
 	return total
 }
