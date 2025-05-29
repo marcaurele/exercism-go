7e4d451d7e792b06989f6ffc6c3598721346e038
Marc-Aurèle Brothier <m@brothier.org>
2025-05-29 22:42:08 +0200

    sol: switch case

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -3,5 +3,25 @@ package space
 type Planet string
 
 func Age(seconds float64, planet Planet) float64 {
-	panic("Please implement the Age function")
+	earthYear := seconds / 31557600.0
+	switch planet {
+	case "Mercury":
+		return earthYear / 0.2408467
+	case "Venus":
+		return earthYear / 0.61519726
+	case "Earth":
+		return earthYear
+	case "Mars":
+		return earthYear / 1.8808158
+	case "Jupiter":
+		return earthYear / 11.862615
+	case "Saturn":
+		return earthYear / 29.447498
+	case "Uranus":
+		return earthYear / 84.016846
+	case "Neptune":
+		return earthYear / 164.79132
+	default:
+		return -1.0
+	}
 }
