4319297205ef2ec9fdded3890c3e57121526a72f
Marc-Aurèle Brothier <m@brothier.org>
2025-05-29 22:24:57 +0200

    sol: algo / formula

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,5 +1,36 @@
 package luhn
 
+import (
+	"strings"
+)
+
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	id = strings.ReplaceAll(id, " ", "")
+
+	lenId := len(id)
+	if lenId <= 1 {
+		return false
+	}
+
+	var total int
+	moduloId := lenId % 2
+
+	for pos, char := range id {
+		val := int(char - '0')
+
+		if val < 0 || val > 9 {
+			return false
+		}
+
+		total += val
+		if pos%2 == moduloId {
+			total += val
+			if val >= 5 {
+				total -= 9
+			}
+		}
+
+	}
+
+	return total%10 == 0
 }
