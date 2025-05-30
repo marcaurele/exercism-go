d70b8b4c3d0fe7f7657ee00ca7969b2528d55dec
Marc-Aurèle Brothier <m@brothier.org>
2025-05-30 09:19:23 +0200

    sol: switch, rune

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,5 +1,18 @@
 package strand
 
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	var result string
+	for _, letter := range dna {
+		switch letter {
+		case 'G':
+			result += "C"
+		case 'C':
+			result += "G"
+		case 'T':
+			result += "A"
+		case 'A':
+			result += "U"
+		}
+	}
+	return result
 }
