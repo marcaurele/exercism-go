29517d0b4591239bce21a3d8a0e22e0f24c795a1
Marc-Aurèle Brothier <m@brothier.org>
2025-06-01 09:57:54 +0200

    sol: generic, function parameter

--- strain/strain.go
+++ strain/strain.go
@@ -5,3 +5,23 @@ package strain
 // You will need typed parameters (aka "Generics") to solve this exercise.
 // They are not part of the Exercism syllabus yet but you can learn about
 // them here: https://go.dev/tour/generics/1
+
+func Keep[T any](list []T, fn func(T) bool) []T {
+	var result []T
+	for _, i := range list {
+		if fn(i) {
+			result = append(result, i)
+		}
+	}
+	return result
+}
+
+func Discard[T any](list []T, fn func(T) bool) []T {
+	var result []T
+	for _, i := range list {
+		if !fn(i) {
+			result = append(result, i)
+		}
+	}
+	return result
+}
