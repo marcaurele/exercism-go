07359ea475a2913fb6deb1800860b68082d044f7
Marc-Aurèle Brothier <m@brothier.org>
2025-06-02 13:03:26 +0200

    sol: extra runes, range

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,16 @@
 package pangram
 
+import "strings"
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	letterSeen := map[rune]bool{}
+	for _, char := range strings.ToLower(input) {
+		if char >= 'a' && char <= 'z' {
+			if _, exists := letterSeen[char]; exists {
+				continue
+			}
+			letterSeen[char] = true
+		}
+	}
+	return len(letterSeen) == 26
 }
