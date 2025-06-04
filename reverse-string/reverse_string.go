1518a2b39db7f991d877dfb395a888c233ff5701
Marc-Aurèle Brothier <m@brothier.org>
2025-06-04 06:55:20 +0200

    sol: runes

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,5 +1,13 @@
 package reverse
 
+import (
+	"unicode/utf8"
+)
+
 func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+	result := []rune(input)
+	for i, j := 0, utf8.RuneCountInString(input)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
+	return string(result)
 }
