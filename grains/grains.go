9e4e099675dfdc95737198a5e5b6f31a519315c2
Marc-Aurèle Brothier <m@brothier.org>
2025-06-06 17:36:40 +0200

    sol: conversion uint64, bit

--- grains/grains.go
+++ grains/grains.go
@@ -1,9 +1,22 @@
 package grains
 
+import (
+	"errors"
+	"math"
+)
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number < 1 || number > 64 {
+		return 0, errors.New("number must be in the range [1,64]")
+	}
+	return uint64(math.Pow(2, float64(number)-1)), nil
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+	var total uint64
+	for i := 1; i <= 64; i++ {
+		t, _ := Square(i)
+		total += t
+	}
+	return total
 }
