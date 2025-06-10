ee96fef04e54c857d41a324b45169f3b958096e4
Marc-Aurèle Brothier <m@brothier.org>
2025-06-10 08:37:25 +0200

    sol: anonymous function

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -1,5 +1,42 @@
 package romannumerals
 
+import (
+	"errors"
+	"strings"
+)
+
 func ToRomanNumeral(input int) (string, error) {
-	panic("Please implement the ToRomanNumeral function")
+	if input < 1 || input > 3999 {
+		return "", errors.New("input must be greater or equal to 1, and less than 3999")
+	}
+
+	transform := func(in int, single string, middle string, max string) string {
+		switch {
+		case in <= 3:
+			return strings.Repeat(single, in)
+		case in == 4:
+			return single + middle
+		case in == 5:
+			return middle
+		case in <= 8:
+			return middle + strings.Repeat(single, in-5)
+		case in == 9:
+			return single + max
+		default:
+			return ""
+		}
+	}
+
+	output := transform(input/1000, "M", "", "")
+
+	next := input % 1000
+	output += transform(next/100, "C", "D", "M")
+
+	next = next % 100
+	output += transform(next/10, "X", "L", "C")
+
+	u := next % 10
+	output += transform(u, "I", "V", "X")
+
+	return output, nil
 }
