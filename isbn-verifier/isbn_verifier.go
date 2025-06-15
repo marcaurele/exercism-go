3ee6ac6446c2f7b946094b8bcb845771d548b916
Marc-Aurèle Brothier <m@brothier.org>
2025-06-15 14:40:13 +0200

    sol: string parsing

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -1,5 +1,26 @@
 package isbn
 
+import "strings"
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	check := 0
+	compactIsbn := strings.TrimSpace(strings.ReplaceAll(isbn, "-", ""))
+
+	if len(compactIsbn) != 10 {
+		return false
+	}
+
+	for i, char := range compactIsbn {
+		if !(char >= '0' && char <= '9' || char == 'X' && i == 9) {
+			return false
+		}
+
+		if char == 'X' {
+			check += 10
+		} else {
+			check += (int(char) - '0') * (10 - i)
+		}
+	}
+
+	return check%11 == 0
 }
