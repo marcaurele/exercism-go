29d2bc39cc992a95676847295d08f6d007ca0d0c
Marc-Aurèle Brothier <m@brothier.org>
2025-06-19 22:05:52 +0200

    sol: runes, range

--- rotational-cipher/rotational_cipher.go
+++ rotational-cipher/rotational_cipher.go
@@ -1,5 +1,16 @@
 package rotationalcipher
 
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+	rot := []rune(plain)
+
+	for i, r := range plain {
+		if r >= 'a' && r <= 'z' {
+			rot[i] = 'a' + (r+rune(shiftKey)-'a')%26
+		} else if r >= 'A' && r <= 'Z' {
+			rot[i] = 'A' + (r+rune(shiftKey)-'A')%26
+		} else {
+			rot[i] = r
+		}
+	}
+	return string(rot)
 }
