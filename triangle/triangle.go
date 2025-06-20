0cff85f02f9623091a78de7729b36b7880b23ab3
Marc-Aurèle Brothier <m@brothier.org>
2025-06-20 21:26:51 +0200

    sol: struct, iota

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -5,24 +5,29 @@
 // https://golang.org/doc/effective_go.html#commentary
 package triangle
 
-
 // Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind uint8
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	// Pick values for the following identifiers used by the test program.
+	NaT Kind = 0 // not a triangle
+	Equ Kind = 1 // equilateral
+	Iso Kind = 2 // isosceles
+	Sca Kind = 3 // scalene
 )
 
 // KindFromSides should have a comment documenting it.
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
 	var k Kind
+	switch {
+	case a <= 0 || b <= 0 || c <= 0 || a+b < c || b+c < a || a+c < b:
+		k = NaT
+	case a == b && b == c:
+		k = Equ
+	case a == b || b == c || a == c:
+		k = Iso
+	default:
+		k = Sca
+	}
 	return k
 }
