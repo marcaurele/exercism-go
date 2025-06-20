198e15de30b925494d412d93b0e08de733ef92dc
Marc-Aurèle Brothier <m@brothier.org>
2025-06-20 18:08:29 +0200

    fixup! map

--- resistor-color-duo/resistor_color_duo.go
+++ resistor-color-duo/resistor_color_duo.go
@@ -1,25 +1,17 @@
 package resistorcolorduo
 
-import "strconv"
-
-// Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
-	codes := map[string]string{
-		"black":  "0",
-		"brown":  "1",
-		"red":    "2",
-		"orange": "3",
-		"yellow": "4",
-		"green":  "5",
-		"blue":   "6",
-		"violet": "7",
-		"grey":   "8",
-		"white":  "9",
+	codes := map[string]int{
+		"black":  0,
+		"brown":  1,
+		"red":    2,
+		"orange": 3,
+		"yellow": 4,
+		"green":  5,
+		"blue":   6,
+		"violet": 7,
+		"grey":   8,
+		"white":  9,
 	}
-	total := ""
-	for i := 0; i < 2; i++ {
-		total += codes[colors[i]]
-	}
-	result, _ := strconv.Atoi(total)
-	return result
+	return codes[colors[0]]*10 + codes[colors[1]]
 }
