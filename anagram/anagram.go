0599a3a6f322ad72d2d3ab7ed6278e8ec511b37e
Marc-Aurèle Brothier <m@brothier.org>
2025-06-22 09:04:54 +0200

    sol: range, runes, strings, sort

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -1,5 +1,26 @@
 package anagram
 
+import (
+	"sort"
+	"strings"
+)
+
 func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+	subjectComp := SortString(subject)
+	subjectLower := strings.ToLower(subject)
+	var results []string
+
+	for _, candidate := range candidates {
+		if subjectComp == SortString(candidate) && subjectLower != strings.ToLower(candidate) {
+			results = append(results, candidate)
+		}
+	}
+
+	return results
+}
+
+func SortString(input string) string {
+	small := strings.Split(strings.ToLower(input), "")
+	sort.Strings(small)
+	return strings.Join(small, "")
 }
