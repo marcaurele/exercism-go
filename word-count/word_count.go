48fd5a8123f57af1d20b0bfffb26f965994fe5a8
Marc-Aurèle Brothier <m@brothier.org>
2025-06-22 12:41:53 +0200

    fixup! regexp, split string

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -3,42 +3,14 @@ package wordcount
 import (
 	"regexp"
 	"strings"
-	"unicode"
 )
 
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
-	splits := regexp.MustCompile(`[\s,]+`).Split(strings.TrimSpace(phrase), -1)
-	var frequencies Frequency = make(Frequency)
-	for _, word := range splits {
-		lower := ExtractWord(word)
-		if lower == "" {
-			continue
-		}
-
-		val, err := frequencies[lower]
-		if !err {
-			frequencies[lower] = 1
-		} else {
-			frequencies[lower] = val + 1
-		}
+	frequencies := make(Frequency)
+	for _, word := range regexp.MustCompile(`\w+('\w+)?`).FindAllString(strings.ToLower(phrase), -1) {
+		frequencies[word]++
 	}
 	return frequencies
-}
-
-func ExtractWord(input string) string {
-	lower := strings.ToLower(input)
-	var output []rune
-	var prev rune
-	for i, char := range lower {
-		if prev == '\'' && (unicode.IsLetter(char) || unicode.IsDigit(char)) && i > 1 {
-			output = append(output, '\'')
-		}
-		if unicode.IsLetter(char) || unicode.IsDigit(char) {
-			output = append(output, char)
-		}
-		prev = char
-	}
-	return string(output)
 }
