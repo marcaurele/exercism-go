ab4e6b441ad401a792e3e0dba405d77a3f10ef3c
Marc-Aurèle Brothier <m@brothier.org>
2025-06-24 08:00:16 +0200

    sol: rune count

--- largest-series-product/largest_series_product.go
+++ largest-series-product/largest_series_product.go
@@ -1,5 +1,36 @@
 package lsproduct
 
+import (
+	"errors"
+	"slices"
+	"unicode/utf8"
+)
+
 func LargestSeriesProduct(digits string, span int) (int64, error) {
-	panic("Please implement the LargestSeriesProduct function")
+	len := utf8.RuneCountInString(digits)
+	if span < 0 {
+		return 0, errors.New("span must not be negative")
+	}
+	if len < span {
+		return 0, errors.New("span must be smaller than string length")
+	}
+
+	products := make([]int64, len)
+	for i := 0; i < len; i++ {
+		digit := int64(digits[i] - '0')
+
+		if digit < 0 || digit > 9 {
+			return 0, errors.New("invalid digit")
+		}
+		if i < len-span+1 {
+			products[i] = digit
+		}
+
+		for j := 0; j < span-1; j++ {
+			if i > j {
+				products[i-j-1] *= digit
+			}
+		}
+	}
+	return slices.Max(products), nil
 }
