097e5f36e507f771d621dafd96b046b25d5ecad8
Marc-Aurèle Brothier <m@brothier.org>
2025-08-11 08:19:24 +0200

    sol: for, range - split function

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -2,27 +2,26 @@ package prime
 
 import "errors"
 
-// Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
+var primes = []int{}
+
+func isPrime(n int) bool {
+	for _, i := range primes {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func Nth(n int) (int, error) {
 	if n <= 0 {
 		return 0, errors.New("there is no zeroth prime")
 	}
 
-	primes := []int{}
 	for i := 2; ; i++ {
-		prime := true
-
-		for _, val := range primes {
-			if i%val == 0 {
-				prime = false
-				break
-			}
-		}
-
-		if prime {
+		if isPrime(i) {
 			primes = append(primes, i)
 		}
-
 		if len(primes) == n {
 			break
 		}
