74f7a82df70d5ee3e637b5cbb8e1c9884ecf9a9c
Marc-Aurèle Brothier <m@brothier.org>
2025-08-13 08:40:58 +0200

    sol: for, if, rune, strings

--- atbash-cipher/atbash_cipher.go
+++ atbash-cipher/atbash_cipher.go
@@ -1,5 +1,25 @@
 package atbash
 
+import "strings"
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	init := []rune(strings.ToLower(s))
+	var result []rune
+	letterSwitched := 0
+	for i := 0; i < len(init); i++ {
+		if init[i] >= 'a' && init[i] <= 'z' {
+			result = append(result, ('z'-init[i])%26+'a')
+			letterSwitched += 1
+			if letterSwitched%5 == 0 {
+				result = append(result, ' ')
+			}
+		} else if init[i] >= '0' && init[i] <= '9' {
+			result = append(result, init[i])
+			letterSwitched += 1
+			if letterSwitched%5 == 0 {
+				result = append(result, ' ')
+			}
+		}
+	}
+	return strings.TrimSpace(string(result))
 }
