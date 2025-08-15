345fb7bd99e53654c5a3403ebfd848559160ae87
Marc-Aurèle Brothier <m@brothier.org>
2025-08-15 08:55:43 +0200

    sol: struct, interface

--- simple-cipher/simple_cipher.go
+++ simple-cipher/simple_cipher.go
@@ -1,32 +1,117 @@
 package cipher
 
+import "strings"
+
 // Define the shift and vigenere types here.
 // Both types should satisfy the Cipher interface.
 
+type caesar struct {
+}
+type shift struct {
+	Distance int
+}
+type vigenere struct {
+	Key string
+}
+
 func NewCaesar() Cipher {
-	panic("Please implement the NewCaesar function")
+	return caesar{}
+}
+
+func (c caesar) Encode(input string) string {
+	var result []rune
+	for _, letter := range strings.ToLower(input) {
+		if letter >= 'a' && letter <= 'z' {
+			result = append(result, 'a'+((letter-'a'+3)%26))
+		}
+	}
+	return string(result)
+}
+
+func (c caesar) Decode(input string) string {
+	var result []rune
+	for _, letter := range strings.ToLower(input) {
+		if letter >= 'a' && letter <= 'z' {
+			result = append(result, 'a'+((letter-'a'+(26-3))%26))
+		}
+	}
+	return string(result)
 }
 
 func NewShift(distance int) Cipher {
-	panic("Please implement the NewShift function")
+	if distance < -25 || distance == 0 || distance > 25 {
+		return nil
+	}
+	return shift{
+		Distance: distance,
+	}
 }
 
 func (c shift) Encode(input string) string {
-	panic("Please implement the Encode function")
+	var result []rune
+	for _, letter := range strings.ToLower(input) {
+		if letter >= 'a' && letter <= 'z' {
+			result = append(result, 'a'+((letter-'a'+26+rune(c.Distance))%26))
+		}
+	}
+	return string(result)
 }
 
 func (c shift) Decode(input string) string {
-	panic("Please implement the Decode function")
+	var result []rune
+	for _, letter := range strings.ToLower(input) {
+		if letter >= 'a' && letter <= 'z' {
+			result = append(result, 'a'+((letter-'a'+(26-rune(c.Distance)))%26))
+		}
+	}
+	return string(result)
 }
 
 func NewVigenere(key string) Cipher {
-	panic("Please implement the NewVigenere function")
+	if len(key) == 0 {
+		return nil
+	}
+	diffThanA := false
+	for _, l := range key {
+		if l < 'a' || l > 'z' {
+			return nil
+		}
+		if !diffThanA && l != 'a' {
+			diffThanA = true
+		}
+	}
+	if !diffThanA {
+		return nil
+	}
+	return vigenere{
+		Key: key,
+	}
 }
 
 func (v vigenere) Encode(input string) string {
-	panic("Please implement the Encode function")
+	var result []rune
+	keyLength := len(v.Key)
+	key := []rune(v.Key)
+	keyMove := 0
+	for _, letter := range strings.ToLower(input) {
+		if letter >= 'a' && letter <= 'z' {
+			result = append(result, 'a'+((letter-'a'+(key[keyMove%keyLength]-'a')%26)%26))
+			keyMove += 1
+		}
+	}
+	return string(result)
 }
 
 func (v vigenere) Decode(input string) string {
-	panic("Please implement the Decode function")
+	var result []rune
+	keyLength := len(v.Key)
+	key := []rune(v.Key)
+	keyMove := 0
+	for _, letter := range strings.ToLower(input) {
+		if letter >= 'a' && letter <= 'z' {
+			result = append(result, 'a'+((26+letter-'a'-(key[keyMove%keyLength]-'a')%26)%26))
+			keyMove += 1
+		}
+	}
+	return string(result)
 }
