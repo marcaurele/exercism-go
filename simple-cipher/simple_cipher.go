253eb2ba004e5beb703f93e5b25c2e17782ff499
Marc-Aurèle Brothier <m@brothier.org>
2025-08-19 21:34:40 +0200

    refactor: struct, interface

--- simple-cipher/simple_cipher.go
+++ simple-cipher/simple_cipher.go
@@ -1,86 +1,39 @@
 package cipher
 
-import "strings"
+import (
+	"strings"
+)
 
-// Define the shift and vigenere types here.
-// Both types should satisfy the Cipher interface.
-
-type caesar struct {
-}
 type shift struct {
 	Distance int
 }
+
 type vigenere struct {
 	Key string
 }
 
 func NewCaesar() Cipher {
-	return caesar{}
-}
-
-func (c caesar) Encode(input string) string {
-	var result []rune
-	for _, letter := range strings.ToLower(input) {
-		if letter >= 'a' && letter <= 'z' {
-			result = append(result, 'a'+((letter-'a'+3)%26))
-		}
-	}
-	return string(result)
-}
-
-func (c caesar) Decode(input string) string {
-	var result []rune
-	for _, letter := range strings.ToLower(input) {
-		if letter >= 'a' && letter <= 'z' {
-			result = append(result, 'a'+((letter-'a'+(26-3))%26))
-		}
-	}
-	return string(result)
+	return NewShift(3)
 }
 
 func NewShift(distance int) Cipher {
-	if distance < -25 || distance == 0 || distance > 25 {
-		return nil
+	if distance < 0 {
+		distance += 26
 	}
-	return shift{
-		Distance: distance,
-	}
-}
-
-func (c shift) Encode(input string) string {
-	var result []rune
-	for _, letter := range strings.ToLower(input) {
-		if letter >= 'a' && letter <= 'z' {
-			result = append(result, 'a'+((letter-'a'+26+rune(c.Distance))%26))
-		}
-	}
-	return string(result)
-}
-
-func (c shift) Decode(input string) string {
-	var result []rune
-	for _, letter := range strings.ToLower(input) {
-		if letter >= 'a' && letter <= 'z' {
-			result = append(result, 'a'+((letter-'a'+(26-rune(c.Distance)))%26))
-		}
-	}
-	return string(result)
+	return NewVigenere(string(rune('a' + distance)))
 }
 
 func NewVigenere(key string) Cipher {
-	if len(key) == 0 {
-		return nil
-	}
-	diffThanA := false
+	onlyA := true
 	for _, l := range key {
 		if l < 'a' || l > 'z' {
 			return nil
 		}
-		if !diffThanA && l != 'a' {
-			diffThanA = true
+		if onlyA && l != 'a' {
+			onlyA = false
 		}
 	}
-	if !diffThanA {
+	if onlyA {
 		return nil
 	}
 	return vigenere{
@@ -89,27 +42,25 @@ func NewVigenere(key string) Cipher {
 }
 
 func (v vigenere) Encode(input string) string {
-	var result []rune
-	keyLength := len(v.Key)
-	key := []rune(v.Key)
-	keyMove := 0
-	for _, letter := range strings.ToLower(input) {
-		if letter >= 'a' && letter <= 'z' {
-			result = append(result, 'a'+((letter-'a'+(key[keyMove%keyLength]-'a')%26)%26))
-			keyMove += 1
-		}
-	}
-	return string(result)
+	return v.shift(input, true)
 }
 
 func (v vigenere) Decode(input string) string {
+	return v.shift(input, false)
+}
+
+func (v vigenere) shift(input string, encode bool) string {
 	var result []rune
 	keyLength := len(v.Key)
 	key := []rune(v.Key)
 	keyMove := 0
 	for _, letter := range strings.ToLower(input) {
 		if letter >= 'a' && letter <= 'z' {
-			result = append(result, 'a'+((26+letter-'a'-(key[keyMove%keyLength]-'a')%26)%26))
+			if encode {
+				result = append(result, 'a'+((letter-'a'+(key[keyMove%keyLength]-'a')%26)%26))
+			} else {
+				result = append(result, 'a'+((26+letter-'a'-(key[keyMove%keyLength]-'a')%26)%26))
+			}
 			keyMove += 1
 		}
 	}
