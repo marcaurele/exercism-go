f810e67e0470e8ed8333b84055fc4854d5ffefe5
Marc-Aurèle Brothier <m@brothier.org>
2025-08-19 21:53:04 +0200

    sol: for, make

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,5 +1,15 @@
 package sieve
 
 func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+	allNumbers := make([]bool, limit+1)
+	var primes []int
+	for i := 2; i <= limit; i++ {
+		if !allNumbers[i] {
+			primes = append(primes, i)
+			for j := 0; j*i <= limit; j++ {
+				allNumbers[j*i] = true
+			}
+		}
+	}
+	return primes
 }
