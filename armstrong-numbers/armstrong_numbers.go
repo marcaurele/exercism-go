d5206358c354dde6d6e23e40b4dddc60bd9c64c5
Marc-Aurèle Brothier <m@brothier.org>
2025-08-20 07:57:21 +0200

    sol: for, append

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,24 @@
 package armstrong
 
+import (
+	"math"
+)
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	var parts []int
+	number := n
+	for {
+		if number < 10 {
+			parts = append(parts, number%10)
+			break
+		}
+		parts = append(parts, number%10)
+		number = number / 10
+	}
+	l := len(parts)
+	total := 0.0
+	for i := 0; i < l; i++ {
+		total += math.Pow(float64(parts[i]), float64(l))
+	}
+	return int(total) == n
 }
