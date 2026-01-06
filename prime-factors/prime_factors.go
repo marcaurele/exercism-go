abc664ac300172d75cba3adcd36abea74dd650f8
Marc-Aurèle Brothier <m@brothier.org>
2026-01-06 21:39:11 +0100

    sol: for, append

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,5 +1,23 @@
 package prime
 
 func Factors(n int64) []int64 {
-	panic("Please implement the Factors function")
+	var primes = []int64{}
+
+	if n < 1 {
+		return []int64{}
+	}
+
+	var i int64
+
+	for i = 2; ; i++ {
+		if n%i == 0 {
+			primes = append(primes, i)
+			n = n / i
+			i = 1
+		}
+		if n == 1 {
+			break
+		}
+	}
+	return primes
 }
