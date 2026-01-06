8e37c57b97f8663bfa19b680df0f38874b459eae
Marc-Aurèle Brothier <m@brothier.org>
2026-01-06 20:46:01 +0100

    sol: regexp

--- phone-number/phone_number.go
+++ phone-number/phone_number.go
@@ -1,13 +1,47 @@
 package phonenumber
 
+import (
+	"errors"
+	"regexp"
+)
+
 func Number(phoneNumber string) (string, error) {
-	panic("Please implement the Number function")
+	nonDigitRegex := regexp.MustCompile(`[^\d]`)
+	nanpRegex := regexp.MustCompile(`^[2-9]\d{2}[2-9]\d{6}$`)
+
+	cleaned := nonDigitRegex.ReplaceAllString(phoneNumber, "")
+
+	if len(cleaned) == 11 {
+		if cleaned[0] != '1' {
+			return "", errors.New("11 digits must start with 1")
+		}
+		cleaned = cleaned[1:]
+	}
+
+	if len(cleaned) != 10 {
+		return "", errors.New("must be 10 or 11 digits")
+	}
+
+	if !nanpRegex.MatchString(cleaned) {
+		return "", errors.New("invalid NANP format")
+	}
+
+	return cleaned, nil
 }
 
 func AreaCode(phoneNumber string) (string, error) {
-	panic("Please implement the AreaCode function")
+	clean, error := Number(phoneNumber)
+	if error != nil {
+		return clean, error
+	}
+	return string([]rune(clean)[:3]), nil
 }
 
 func Format(phoneNumber string) (string, error) {
-	panic("Please implement the Format function")
+	clean, error := Number(phoneNumber)
+	if error != nil {
+		return clean, error
+	}
+	phone := []rune(clean)
+	return "(" + string(phone[:3]) + ") " + string(phone[3:6]) + "-" + string(phone[6:]), nil
 }
